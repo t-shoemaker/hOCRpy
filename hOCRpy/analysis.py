ad5b4975df17083fab2deb9dc9463e1e6e410064
Tyler Shoemaker <tshoemaker@ucdavis.edu>
2022-10-08 21:27:13 -0700

    using a custom linear regression model

--- hOCRpy/analysis.py
+++ hOCRpy/analysis.py
@@ -3,9 +3,8 @@
 
 from .page import hOCR
 from .clustering import KMeans, ClusterResults, silhouette_score
+from .linregress import LinearRegression
 import numpy as np
-import scipy.stats
-from scipy.stats._stats_mstats_common import LinregressResult
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 
@@ -15,7 +14,7 @@ class CoordData:
         """Extract the bounding boxes from a hOCR page."""
         arr = np.array(hocr.bboxes)
         # Store in a dictionary, along with the indicies
-        self.coords = {
+        self.data = {
             'x0': arr[:,0],
             'y0': arr[:,1],
             'x1': arr[:,2],
@@ -23,7 +22,14 @@ class CoordData:
             'indices': np.array(range(len(arr)))
         }
 
-    def fit_lm(self, x: str='', y: str=''):
+    def fit_lm(
+        self, x: str='',
+        y: str='',
+        test_size: float=.2,
+        alpha: float=0.5,
+        tol: float=1e-5,
+        iters: int=500
+    ) -> None:
         """Fit a least-squares linear regression model to the coordinates.
 
         Parameters
@@ -32,20 +38,27 @@ class CoordData:
             Dimension 1 (e.g. x0, y0, etc.)
         y
             Dimension 2 (e.g. x1, y1, etc.)
+        test_size
+            The proportional size of the testing data
+        alpha
+            Learning rate
+        tol
+            The tolerance at which to stop training
+        iters
+            The iterations at which to stop training
 
         Raises
         ------
             ValueError if the coordinates are not in the data
         """
-        if x not in self.coords or y not in self.coords:
-            problem = ', '.join(list(self.coords.keys()))
-            raise ValueError(f"Valid coordinate data: {problem}")
+        if x not in self.data or y not in self.data:
+            valid = ', '.join(list(self.data.keys()))
+            raise ValueError(f"Valid coordinate data: {valid}")
 
-        x, y = self.coords[x], self.coords[y]
-        # Fit the model
-        self.lm = scipy.stats.linregress(x, y)
-        # Make a vector coordinates to represent the regression line
-        self.regression_line = self.lm.slope * x + self.lm.intercept
+        x, y = self.data[x], self.data[y]
+        model = LinearRegression(x, y)
+        model.train(test_size, alpha, tol, iters)
+        self.lm = model
 
     def cluster(
         self,
@@ -74,12 +87,12 @@ class CoordData:
         ------
             ValueError if the specified dimensions are not in the data
         """
-        if dim1 not in self.coords or dim2 not in self.coords:
-            problem = ', '.join(list(self.coords.keys()))
-            raise ValueError(f"Valid dimensions: {problem}")
+        if dim1 not in self.data or dim2 not in self.data:
+            validm = ', '.join(list(self.data.keys()))
+            raise ValueError(f"Valid dimensions: {valid}")
 
         # Get the data to cluster on
-        to_cluster = list(zip(self.coords[dim1], self.coords[dim2]))
+        to_cluster = list(zip(self.data[dim1], self.data[dim2]))
         self.cluster_on = np.array(to_cluster)
 
         # Initialize and fit a k-means clusterer
@@ -87,7 +100,15 @@ class CoordData:
         self.clusters = kmeans.predict(self.cluster_on)
         self.labels = kmeans.labels
 
-def lm_model(hocr, x='indices', y='x0') -> LinregressResult:
+def lm_model(
+    hocr: hOCR,
+    x: str='indices',
+    y: str='x0',
+    test_size: float=.2,
+    alpha: float=0.5,
+    tol: float=1e-5,
+    iters: int=500
+) -> LinearRegression:
     """Return a least-squares linear regression model.
 
     Parameters
@@ -96,6 +117,14 @@ def lm_model(hocr, x='indices', y='x0') -> LinregressResult:
         Dimension 1 (e.g. x0, y0, etc.)
     y
         Dimension 2 (e.g. x1, y1, etc.)
+    test_size
+        The proportional size of the testing data
+    alpha
+        Learning rate
+    tol
+        The tolerance at which to stop training
+    iters
+        The iterations at which to stop training
 
     Returns
     -------
@@ -149,12 +178,11 @@ def predict_columns(
     data = CoordData(hocr)
 
     # Before we do any predictions, we fit a linear model to the data to find
-    # its R squared value (the proportion of variation in the data based on the
+    # its R-Squared value (the proportion of variation in the data based on the
     # predicted variable of a linear model). If that value is below the `r2`
     # cutoff, the page is considered to be a single column
     data.fit_lm(dim1, dim2)
-    data_r2 = data.lm.rvalue ** 2
-    if data_r2 < r2:
+    if data.lm.rsquared < r2:
         return 1
 
     # Grid search the k space
@@ -177,6 +205,10 @@ def bbox_plot(
     x: str='indices',
     y: str='x0',
     regression_line: bool=True,
+    test_size: float=.2,
+    alpha: float=0.5,
+    tol: float=1e-5,
+    iters: int=500,
     figx: int=12,
     figy: int=9,
     title: str=''
@@ -193,6 +225,14 @@ def bbox_plot(
         Data for the Y axis
     regression_line
         Whether to fit a linear model and plot the regression line
+    test_size
+        The proportional size of the testing data
+    alpha
+        Learning rate
+    tol
+        The tolerance at which to stop training
+    iters
+        The number of iteratinos at which to stop training
     figx
         Figure width
     figy
@@ -206,15 +246,15 @@ def bbox_plot(
         Linear plot
     """
     data = CoordData(hocr)
-    x_data, y_data = data.coords[x], data.coords[y]
+    x_data, y_data = data.data[x], data.data[y]
 
     fig, ax = plt.subplots(figsize=(figx, figy))
     plt.plot(x_data, y_data)
 
     # Optionally add a regression line
     if regression_line:
-        data.fit_lm(x, y)
-        plt.plot(x_data, data.regression_line, c='red', label="Regression line")
+        data.fit_lm(x, y, test_size, alpha, tol, iters)
+        plt.plot(x_data, data.lm.regression_line, c='red', label="Regression line")
         plt.legend()
 
     plt.title(title)
@@ -260,11 +300,11 @@ def cluster_plot(
     plot
         Scatter plot of clusters
     """
-    data = CoordData(hocr)
-    data.cluster(k, dim1=dim1, dim2=dim2, tol=tol, iters=iters)
+    coords = CoordData(hocr)
+    coords.cluster(k, dim1=dim1, dim2=dim2, tol=tol, iters=iters)
 
     fig, ax = plt.subplots(figsize=(figx, figy))
-    plt.scatter(data.coords[dim1], data.coords[dim2], c=data.labels)
+    plt.scatter(coords.data[dim1], coords.data[dim2], c=coords.labels)
     plt.title(title)
     plt.xlabel(dim1)
     plt.ylabel(dim2)
---
+++ hOCRpy/linregress.py
@@ -0,0 +1,183 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import numpy as np
+from typing import Tuple
+
+class LinearRegression:
+
+    def __init__(self, x: np.array, y: np.array):
+        """Initialize by binding X and Y data.
+
+        Adapted from
+        https://www.cs.toronto.edu/~frossard/post/linear_regression/
+        """
+        # Ensure the data are floats
+        if x.dtype != float:
+            x = x.astype('float64')
+            y = y.astype('float64')
+
+        # Load in the X values and normalize them
+        self.x = x[:, np.newaxis]
+        self.x /= np.max(x)
+        self.x = np.hstack((np.ones_like(self.x), self.x))
+        self.y = y
+        self.num_obvs = len(x)
+
+        # The following will be modified during training: the gradient values,
+        # the coeffiecients (which we randomly populate for now), the cost
+        # function, the R-squared value, the slope, and the regression line. We
+        # also set a flag for whether the model is trained and whether it has
+        # converged
+        self.gradient = np.zeros(2)
+        self.w = np.random.random(2)
+        self.mse = 0.
+        self.test_mse = 0.
+        self.rsquared = 0.
+        self.slope = 0.
+        self.regression_line = np.zeros(self.num_obvs)
+
+        self.trained = False
+        self.converged = False
+
+    def __repr__(self):
+        output = "Regression model\n"
+        output += f"+ Trained: {self.trained}\n"
+        output += f"+ Converged: {self.converged}\n"
+        output += f"+ Test error: {self.test_mse:0.4f}\n"
+        output += f"+ Slope: {self.slope:0.4f}\n"
+        output += f"+ R-Squared: {self.rsquared:0.4f}"
+
+        return output
+
+    @staticmethod
+    def _cost(w: np.array, x: np.array, y: np.array) -> Tuple[np.array, float]:
+        """Calculate gradient descent.
+
+        Parameters
+        ----------
+        w
+            Coefficients
+        x
+            Dimension 1
+        y
+            Dimension 2
+
+        Returns
+        -------
+        gradient
+            Gradient
+        mse
+            Mean-squared error
+        """
+        y_estimate = (x @ w).flatten()
+        error = y.flatten() - y_estimate
+        mse = (1.0 / len(x)) * np.sum(np.power(error, 2))
+        gradient = -(1.0 / len(x)) * (error @ x)
+
+        return gradient, mse
+
+    @staticmethod
+    def _rsquared(pred: np.array, actual: np.array) -> float:
+        """Calculate the R-squared value for a regression.
+
+        Predictions
+        -----------
+        pred
+            Predicted points along the regression line
+        actual
+            Actual points
+
+        Returns
+        -------
+        r
+            The R-squared value
+        """
+        r = np.sum((pred - actual.mean()) ** 2) /\
+            np.sum((actual - actual.mean()) ** 2)
+
+        return r
+
+    def split_data(self, test_size: float=0.2) -> Tuple[np.array]:
+        """Split data into train and test batches.
+
+        Parameters
+        ----------
+        test_size
+            The proportional size of the testing data
+
+        Returns
+        -------
+        batched
+            Data split into train/test batches
+    
+        Raises
+        ------
+            ValueError if the test_size is >=1
+        """
+        if test_size >= 1:
+            raise ValueError("`test_size` must be less than 1")
+
+        # Shuffle the data and find the index at which to make the split
+        order = np.random.permutation(self.num_obvs)
+        portion = int(round(test_size * self.num_obvs))
+        # Split
+        test_x, train_x = self.x[order[:portion]], self.x[order[portion:]]
+        test_y, train_y = self.y[order[:portion]], self.y[order[portion:]]
+
+        return test_x, train_x, test_y, train_y
+
+    def train(
+        self,
+        test_size: float=.2,
+        alpha: float=0.5,
+        tol: float=1e-5,
+        iters: int=500
+    ) -> None:
+        """Train the model.
+
+        Parameters
+        ----------
+        test_size
+            The proportional size of the testing data
+        alpha
+            Learning rate
+        tol
+            The tolerance at which to stop training
+        iters
+            The iterations at which to stop training
+        """
+        # Split the data and create a counter
+        test_x, train_x, test_y, train_y = self.split_data(test_size)
+        i = 0
+
+        while True:
+            # Calculate the cost, then update the weights with the learning
+            # rate and new gradient
+            self.gradient, self.mse = self._cost(self.w, train_x, train_y)
+            new_w = self.w - alpha * self.gradient
+
+            # If we're below the tolerance, calculate the cost on the holdout
+            # data and break
+            if np.sum(abs(new_w - self.w)) < tol:
+                self.converged = True
+                break
+
+            # If we're above the desired iterations, break
+            if iters > iters:
+                break
+
+            i += 1
+            self.w = new_w
+
+        # Set the trained flag
+        self.trained = True
+
+        # Calculate the cost on the testing data
+        _, self.test_mse = self._cost(self.w, test_x, test_y)
+
+        # Set the regression line, its slope, and the R-squared value
+        self.regression_line = self.x @ self.w
+        self.slope = np.gradient(self.regression_line).mean()
+        self.rsquared = self._rsquared(self.regression_line, self.y)
+       
